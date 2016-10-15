546aff449c42b4bc80436fc457fb4e9b9eb45084
blueish4 <michael@hathaways.me.uk>
2016-10-15 16:02:29 +0000

    bigquery API key

--- database.py
+++ database.py
@@ -13,15 +13,13 @@ with SSHTunnelForwarder(ssh_address_or_host=("138.68.141.107", 22), ssh_password
     with open('/home/michael/Documents/ChicagoCrimeData_sample.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            # This will itterate through each row in the file, refer to as dictionary with the headers as keys
-            # ID,Case Number,Date,Block,IUCR,Primary Type,Description,Location Description,Arrest,Domestic,Beat,District,
-            # Ward,Community Area,FBI Code,X Coordinate,Y Coordinate,Year,Updated On,Latitude,Longitude,Location
-            try:
-                cur.execute("INSERT INTO locations(IUCR, longitude, latitude, date) VALUES (%s, %s, %s, STR_TO_DATE(%s, %s));",
-                        (row["IUCR"], row["Longitude"], row["Latitude"], row["Date"], "%m/%d/%Y %h:%i"))
-                connection.commit()
-            except KeyboardInterrupt:
-                pass
+            # This will iterate through each row in the file, refer to as dictionary with the headers as keys
+            # ID,Case Number,Date,Block,IUCR,Primary Type,Description,Location Description,Arrest,Domestic,Beat,
+            # District, Ward,Community Area,FBI Code,X Coordinate,Y Coordinate,Year,Updated On,Latitude,Longitude,
+            # Location
+            cur.execute("INSERT INTO locations(IUCR, longitude, latitude, date) VALUES (%s, %s, %s, STR_TO_DATE(%s, %s));",
+                    (row["IUCR"], row["Longitude"], row["Latitude"], row["Date"], "%m/%d/%Y %h:%i"))
+            connection.commit()
     cur.execute("SELECT * FROM locations")
     print(cur.fetchall())
     connection.commit()
