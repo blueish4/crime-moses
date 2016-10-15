6d7701e13a10066ad3584768d3a3707cbd41b7fc
iwishiwasaneagle <jh.ewers@gmail.com>
2016-10-15 05:58:05 +0100

    ouyiuh

--- database.py
+++ database.py
@@ -1,17 +1,24 @@
 #Main database
 
+
+
 #Input: Location, Datetime, Route, Crimes
 
 import MySQLdb, sys
 from sshtunnel import SSHTunnelForwarder
 
+print("fuck1")
+
 with SSHTunnelForwarder(ssh_address_or_host=("138.68.141.107", 22), ssh_password="django-m0ses", ssh_username="root",
                         remote_bind_address=("localhost", 3306)) as server:
-    connection = MySQLdb.connect(host="127.0.0.1", port=server.local_bind_port, user="python", passwd="python", db="crimemoses")
+    connection = MySQLdb.connect(host="localhost", port=server.local_bind_port, user="python", passwd="python", db="crimemoses")
 
-    cur = connection.cursor()
-    cur.execute("SELECT VERSION()")
+#connection = MySQLdb.connect(host="138.68.141.107", port=3306, user="python", passwd="python",db="crimemoses")
 
-    ver = cur.fetchone()
+cur = connection.cursor()
+cur.execute("SELECT VERSION()")
 
-    print("Database version : %s " % ver)
+ver = cur.fetchone()
+
+print("Database version : %s " % ver)
+print("fuck")
