f37a76c55a872619f05c81c58a415c1eaafab990
blueish <blueish4@users.noreply.github.com>
2016-10-15 04:43:24 +0000

    database connection established

--- database.py
+++ database.py
@@ -5,27 +5,13 @@
 import MySQLdb, sys
 from sshtunnel import SSHTunnelForwarder
 
-with SSHTunnelForwarder(ssh_address_or_host=("138.68.141.107", 22), ssh_password="django-m0ses", ssh_username="root", remote_bind_address=("localhost", 3306)) as server:
-    #try:
-        connection = MySQLdb.connect(host="localhost", port=3306, user="python", passwd="python", db="crimemoses")
+with SSHTunnelForwarder(ssh_address_or_host=("138.68.141.107", 22), ssh_password="django-m0ses", ssh_username="root",
+                        remote_bind_address=("localhost", 3306)) as server:
+    connection = MySQLdb.connect(host="127.0.0.1", port=server.local_bind_port, user="python", passwd="python", db="crimemoses")
 
-        cur = connection.cursor()
-        cur.execute("SELECT VERSION()")
+    cur = connection.cursor()
+    cur.execute("SELECT VERSION()")
 
-        ver = cur.fetchone()
+    ver = cur.fetchone()
 
-        print("Database version : %s " % ver)
-
-    #except:
-
-    #    print("There was an error")
-
-
-
-
-
-
-    #finally:
-
-        if connection:
-             connection.close()
+    print("Database version : %s " % ver)
